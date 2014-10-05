3f81d6ed5fa24f1568bae5a13c04625fef80ac3b
tako0910 <tako.oct0910@gmail.com>
2014-10-05 09:23:26 +0900

    Add files of multirom

---
+++ mr_init_devices.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+// These are paths to folders in /sys which contain "uevent" file
+// need to init this device.
+// MultiROM needs to init framebuffer, mmc blocks, input devices,
+// some ADB-related stuff and USB drives, if OTG is supported
+// You can use * at the end to init this folder and all its subfolders
+const char *mr_init_devices[] =
+{
+    "/sys/class/graphics/fb0",
+
+    "/sys/block/mmcblk0",
+    "/sys/devices/platform/msm_sdcc.1",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p36",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p40",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p38",
+
+    "/sys/bus/mmc",
+    "/sys/bus/mmc/drivers/mmcblk",
+    "/sys/bus/sdio/drivers/bcmsdh_sdmmc",
+    "/sys/module/mmc_core",
+    "/sys/module/mmcblk",
+
+    "/sys/devices/virtual/input*",
+    "/sys/class/misc/uinput",
+
+    // for adb
+    "/sys/class/tty/ptmx",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p38", //system
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p39", // /cache
+    "/sys/class/misc/android_adb",
+    "/sys/class/android_usb/android0/f_adb",
+    "/sys/bus/usb",
+
+    // USB Drive is in here
+    "/sys/devices/platform/msm_hsusb_host*",
+
+    // /dev/fuse
+    "/sys/devices/virtual/misc/fuse",
+
+    NULL
+};
